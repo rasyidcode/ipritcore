1fb0057eee170c1f3782f59c3fe4f25ac92bd4fa
jamil <jamil@rschooltoday.com>
2023-09-26 15:06:19 +0700

    delete migration dir

--- prisma/migrations/20230918023613_update_transaction_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Transaction` MODIFY `updatedAt` DATETIME(3) NULL;
--- prisma/migrations/20230919090820_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Transaction` MODIFY `createdAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3);
