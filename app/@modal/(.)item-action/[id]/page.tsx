ae163963c8b5d3a8017458eec7400fb96f88d341
jamil <jamil@rschooltoday.com>
2023-10-06 11:42:22 +0700

    fix issue with experimental_useFormState

--- app/@modal/(.)item-action/[id]/page.tsx
+++ app/@modal/(.)item-action/[id]/page.tsx
@@ -1,31 +1,57 @@
 'use client'
 
-import ActionFormDeleteButton from "@/components/ActionFormDeleteButton";
+// import ActionFormDeleteButton from "@/components/ActionFormDeleteButton";
 import Modal from "@/components/Modal"
-import { deleteExpense } from "@/utils/actions";
-import { redirect, useRouter } from "next/navigation";
+// import { deleteExpense } from "@/utils/actions";
+import { useRouter } from "next/navigation";
 import { FaPencilAlt, FaSpinner, FaTrash } from "react-icons/fa"
-import { experimental_useFormState as useFormState } from 'react-dom'
-import { revalidatePath } from "next/cache";
+// import { experimental_useFormState as useFormState } from 'react-dom'
+import { useState } from "react";
 
-const initialState = {
-    success: null,
-    message: null
-}
+// const initialState = {
+//     success: null,
+//     message: null
+// }
 
 const ItemAction = ({ params }: {
     params: { id: string }
 }) => {
     const router = useRouter()
-    const [state, deleteFormAction] = useFormState(deleteExpense, initialState)
+    const [deleting, setDeleting] = useState(false)
+    // const [state, deleteFormAction] = useFormState(deleteExpense, initialState)
 
     // WARNING: temporary solution to handle modal close and refresh data
-    if (state.success) {
-        router.refresh()
+    // if (state.success) {
+    //     router.refresh()
         
-        setTimeout(() => {
-            router.back()
-        }, 250);
+    //     setTimeout(() => {
+    //         router.back()
+    //     }, 250);
+    // }
+
+    const onDeleteExpense = async () => {
+        try {
+            setDeleting(true)
+
+            const response = await fetch(`/api/expenses/${params.id}`, {
+                method: 'DELETE'
+            })
+            
+            if (response.ok) {
+                // WARNING: temporary solution to handle modal close and refresh data
+                router.refresh()
+
+                setTimeout(() => {
+                    router.back()
+                }, 250);
+            }
+        } catch (error) {
+            console.log(error)
+
+            alert('Something went wrong')
+        } finally {
+            setDeleting(false)
+        }
     }
 
     return (
@@ -47,10 +73,21 @@ const ItemAction = ({ params }: {
                         }}>
                         <FaPencilAlt /> <span>Edit</span>
                     </button>
-                    <form action={deleteFormAction}>
+                    
+                    <button
+                        type="submit"
+                        className="rounded-md border border-red-100 text-red-500 font-semibold hover:bg-red-100/60 px-2 transition duration-150 ease-in-out flex justify-center items-center gap-2"
+                        disabled={deleting}
+                        aria-disabled={deleting}
+                        onClick={onDeleteExpense}>
+                        {!deleting ? (<><FaTrash /> <span>Delete</span></>) : (
+                            <><div className="animate-spin"><FaSpinner /></div> <span>Deleting...</span></>
+                        )}
+                    </button>
+                    {/* <form action={deleteExpense}>
                         <input type="hidden" name="id" defaultValue={params.id} />
                         <ActionFormDeleteButton />
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </Modal>
