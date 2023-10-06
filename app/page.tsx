d18b2314e0a1fe21c7a7f4f53e89dd84e6b94d81
jamil <jamil@rschooltoday.com>
2023-10-06 11:15:57 +0700

    deploy update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from "../utils/db"
 import { FaAngry, FaPlus } from "react-icons/fa"
 import ExpenseItemNew from "@/components/ExpenseItemNew"
 import Link from "next/link"
-import { Transaction } from "@prisma/client"
+import { Transaction, TransactionType } from "@prisma/client"
 import CenteredMessage from "@/components/CenteredMessage"
 
 const ExpenseItemList = ({ expenses }: { expenses: Transaction[] }) => {
@@ -16,11 +16,36 @@ const ExpenseItemList = ({ expenses }: { expenses: Transaction[] }) => {
 export default async function Home() {
   const currentDate = new Date()
   const expenses = await prisma.transaction.findMany({ where: { date: currentDate } })
+  const expensesTotal = await prisma.transaction.aggregate({ 
+    _sum: {
+      amount: true
+    },
+    where: {
+      type: TransactionType.EXPENSES,
+      date: currentDate
+    }
+   })
+  const incomeTotal = await prisma.transaction.aggregate({ 
+    _sum: {
+      amount: true
+    },
+    where: {
+      type: TransactionType.INCOME,
+      date: currentDate
+    }
+   })
+
+  console.log(incomeTotal)
 
   return (
     <div className="flex flex-col h-full">
       <div className="flex justify-between items-center">
-        <h3 className="font-bold text-teal-500">Thursday, October 05</h3>
+        {/* <h3 className="font-bold text-teal-500">Thursday, October 05</h3> */}
+        <h3 className="font-bold text-teal-500">{`${currentDate.toLocaleDateString('en-US', {
+          weekday: "long",
+          month: "short",
+          day: "numeric"
+        })}`}</h3>
         <Link href="/create-expense" className="text-sm flex justify-center items-center gap-2 text-teal-500 border border-teal-500 px-2 py-1 hover:bg-teal-100/60 transition-all ease-in-out duration-150 font-medium"><FaPlus /> <span>Create New</span></Link>
       </div>
 
@@ -33,7 +58,7 @@ export default async function Home() {
           <div className="bg-white w-full flex flex-col justify-center items-center py-1">
             <h4 className="font-semibold">Expense</h4>
             <p className="font-extrabold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
-              Rp500,000
+              {expensesTotal._sum.amount != null ? `Rp${expensesTotal._sum.amount?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}` : `Rp0`}
             </p>
           </div>
         </div>
@@ -41,7 +66,7 @@ export default async function Home() {
           <div className="bg-white w-full flex flex-col justify-center items-center py-1">
             <h4 className="font-semibold">Income</h4>
             <p className="font-extrabold bg-gradient-to-r from-green-500 to-teal-500 bg-clip-text text-transparent">
-              Rp500,000
+            {incomeTotal._sum.amount != null ? `Rp${incomeTotal._sum.amount?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}` : `Rp0`}
             </p>
           </div>
         </div>
