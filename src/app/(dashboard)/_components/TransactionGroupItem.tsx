b4867dcea5d2e2c716e5a1ae5d718f918ce58622
rasyidcode <rasyidcode7@gmail.com>
2025-03-29 12:40:33 +0700

    #11 update date

--- src/app/(dashboard)/_components/TransactionGroupItem.tsx
+++ src/app/(dashboard)/_components/TransactionGroupItem.tsx
@@ -3,6 +3,7 @@ import { dateToReadableFormat } from "@/lib/stringUtils";
 export default function TransactionGroupItem({ date }: { date: string }) {
   const newDate = new Date();
   newDate.setTime(Date.parse(date));
+
   return (
     <div className="p-2 bg-gray-100">
       <h3 className="text-gray-600 text-sm font-semibold">
--- src/lib/stringUtils.ts
+++ src/lib/stringUtils.ts
@@ -3,9 +3,10 @@ export const numberToIDRFormat = (num: number): string => {
 };
 
 export const dateToReadableFormat = (date: Date): string => {
-  return `${date.toLocaleDateString("en-US", {
+  return `${date.toLocaleDateString("id-ID", {
     weekday: "long",
     month: "short",
     day: "numeric",
+    year: "numeric",
   })}`;
 };
