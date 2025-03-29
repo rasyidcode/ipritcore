a2cca3f9a48b634372e28d86a5a1bef2d6638be1
rasyidcode <rasyidcode7@gmail.com>
2025-03-29 13:04:37 +0700

    #11 add logout button

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import LogoutButton from "./_components/LogoutButton";
 
 export default function DashboardLayout({
   children,
@@ -7,31 +8,36 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="h-screen flex flex-col">
-      <nav className="p-4 flex items-center justify-between">
-        <h1 className="uppercase text-xl">
-          Iprit<strong>Core</strong>
-        </h1>
-        <ul className="inline-flex gap-4 p-2 rounded-lg border">
-          <li>
-            <Link
-              href="/"
-              className="font-semibold bg-gray-100 rounded-lg
+      <nav className="p-4">
+        <div className="flex items-center justify-between rounded-lg border p-2">
+          <h1 className="uppercase text-xl">
+            Iprit<strong>Core</strong>
+          </h1>
+          <ul className="inline-flex gap-2 items-center">
+            <li>
+              <Link
+                href="/"
+                className="font-semibold bg-gray-100 rounded-lg
             px-2 py-1 hover:bg-gray-100 transition-colors
             duration-150 ease-linear text-sm"
-            >
-              Dasbor
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/transactions"
-              className="rounded-lg transition-colors duration-150 ease-linear
+              >
+                Dasbor
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/transactions"
+                className="rounded-lg transition-colors duration-150 ease-linear
             px-2 py-1 hover:bg-gray-100 font-semibold text-sm"
-            >
-              Transaksi
-            </Link>
-          </li>
-        </ul>
+              >
+                Transaksi
+              </Link>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </div>
       </nav>
       {children}
     </div>
