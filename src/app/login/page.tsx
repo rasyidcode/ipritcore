0bed1ba886ef2dae50639fbfc96b17a5c80e8fcf
rasyidcode <rasyidcode7@gmail.com>
2025-03-29 13:19:33 +0700

    #11 update login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,25 @@
-// import { authOptions } from "@/lib/auth";
-// import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import FormLogin from "./_components/FormLogin";
+import GoogleLogin from "./_components/GoogleLogin";
 
 export default async function LoginPage() {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (session?.user) {
-  //   redirect("/");
-  // }
+  if (session?.user) {
+    redirect("/");
+  }
 
   return (
     <div
-      className="flex min-h-screen flex-1 flex-col justify-center
+      className="flex min-h-screen flex-col justify-center
     px-6 py-12 lg:px-8"
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h1 className="uppercase max-w-max mx-auto text-xl">
-          Iprit<strong>Core</strong>
+          Iprit<strong className="text-blue-600">Core</strong>
         </h1>
         <h2 className="mt-10 text-base font-bold tracking-tight text-gray-900">
           Sign in to your account
@@ -24,67 +27,7 @@ export default async function LoginPage() {
       </div>
 
       <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action="#" method="POST" className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                autoComplete="current-password"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3
-              py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500
-              focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-              focus-visible:outline-indigo-600 transition-colors duration-150 ease-in-out"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+        <FormLogin />
 
         <div className="mt-5 flex items-center gap-4">
           <hr className="flex-1" />
@@ -92,35 +35,8 @@ export default async function LoginPage() {
           <hr className="flex-1" />
         </div>
 
-        <div className="mt-6">
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6
-            font-semibold shadow-sm focus-visible:outline
-            focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-blue:outline-indigo-600 border hover:bg-gray-100
-            transition-colors duration-150 ease-in-out items-center gap-2"
-          >
-            <svg viewBox="0 0 24 24" aria-hidden="true" className="size-6">
-              <path
-                d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
-                fill="#EA4335"
-              ></path>
-              <path
-                d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
-                fill="#4285F4"
-              ></path>
-              <path
-                d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
-                fill="#FBBC05"
-              ></path>
-              <path
-                d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
-                fill="#34A853"
-              ></path>
-            </svg>
-            Sign in with Google
-          </button>
+        <div className="mt-5">
+          <GoogleLogin />
         </div>
       </div>
     </div>
